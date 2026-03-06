10f40e2328e6522cbdc94c57f019667900678b1c
RestartB <79792966+RestartB@users.noreply.github.com>
2026-03-06 01:49:06 +0000

    fix: make bouncer criterion buttons have cursor-pointer

--- src/lib/components/bouncer/CriterionTile.svelte
+++ src/lib/components/bouncer/CriterionTile.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import FullscreenOverlay from '$lib/components/ui/FullscreenOverlay.svelte';
   import { Cog, X } from '@lucide/svelte';
   import type { BouncerCriterionSchema } from '$lib/validators/bouncer';
   import CriterionPreferences from './CriterionPreferences.svelte';
@@ -21,11 +20,11 @@
 
 <div class="flex items-center justify-center gap-2 rounded-lg border-2 border-zinc-600 bg-zinc-700 p-1 px-2 text-base">
   {#if criterion.type !== 'avatar'}
-    <button onclick={() => (overlayOpen = true)} aria-label="Open criterion preferences">
+    <button onclick={() => (overlayOpen = true)} aria-label="Open criterion preferences" class="cursor-pointer">
       <Cog size={16} />
     </button>
   {/if}
-  <button onclick={deleteThis} aria-label="Delete criterion">
+  <button onclick={deleteThis} aria-label="Delete criterion" class="cursor-pointer">
     <X size={16} />
   </button>
   <p class="capitalize">{criterion.type.replaceAll('_', ' ')}</p>
