d56a664ae464152e71977076f544530bad396c58
RestartB <79792966+RestartB@users.noreply.github.com>
2026-04-12 16:07:33 +0100

    feat: more work on web leaderboard layout

---
+++ src/routes/public/lb/+page.server.ts
@@ -0,0 +1,10 @@
+import { error } from '@sveltejs/kit';
+import { dev } from '$app/environment';
+
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+  if (!dev) {
+    throw error(404, 'Page not found');
+  }
+};
